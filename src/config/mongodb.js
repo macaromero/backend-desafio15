c2b25fbf3006533231e001c9bcc247c7737cedbb
macarenaromero <macarenasromero@gmail.com>
2022-08-26 14:19:51 -0300

    Mongo

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const mongoConnection = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_CS);
+        await mongoose.connect(process.env.MONGODB_CS, (error) => {
+            console.log(error)
+        });
         console.log("Conexión a base de datos mongodb establecida");
     } catch (error) {
         console.log("Ocurrió un error al conectarse a la base de datos de mongodb", error);
